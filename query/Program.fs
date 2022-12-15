5e7f2fd5e26e44548554596fa5f4e73d0dbb46c3
Augusto Pieper <augustoopieper@gmail.com>
2022-12-14 22:45:59 -0300

    query language tutorial

---
+++ query/Program.fs
@@ -0,0 +1,13 @@
+﻿open Query.Query
+
+let input =
+    """
+filterBy Category = 'Fantasy'
+orderBy Rating desc
+skip 5
+take 10
+"""
+
+match parse input with
+| Result.Ok res -> printfn "%O" res
+| Result.Error err -> printfn "%s" err
---
+++ query/README.md
@@ -0,0 +1,1 @@
+[Learning F# by Designing Your Own Language by Oleksii Holub](https://www.youtube.com/watch?v=34C_7halqGwhttps://www.youtube.com/watch?v=HuN94qNwQmM)
